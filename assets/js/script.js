f7150f7281dcc501d7b8a6740e4605107fe7e7fa
byron4455 <byronstephenson1@gmail.com>
2022-01-23 22:49:43 -0500

    getting there

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,37 @@
-// Assignment code herenvar specialChars = ["!", "@", "#", "$", "%","&"]
+// Assignment code heren
+
+  
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+let passwordLength = prompt("How many characters would you liek in your password? Must be between 8 and 128 characters.");
+     if (passwordLength >= 8 || passwordLength <= 128) {
+       let wantsSpecial = confirm("If you'd like special characters, click okay");
+      let wantsLower = confirm("If you'd like lowercase characters click okay.");
+      let wantsUpper = confirm("If you'd like uppercase characters click okay.");
+      let wantsNumber = confirm("If you'd like numbers click okay.");
+     if (wantsUpper) {
+         passwordText =+ getRandomNumber() + getRandomSpecial();
+     }}
+     return passwordText;
+};
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+};
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var lowercase = ["a", "b", "c", "d","e","f","g","h"]
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H"]
+var specialChars = ["!", "@", "#", "$", "%","&"]
 
 
 function getRandomSpecial() {
@@ -16,45 +46,6 @@ function getRandomSpecial() {
   function getRandomUppercase() {
     return uppercase[Math.floor(Math.random() * uppercase.length)];
   }
-  const randomFunction = {
-    lower: getRandomLowercase,
-    upper: getRandomUppercase,
-    number: getRandomNumber,
-    special: getRandomSpecial
-  };
-  
+//  console.log(writePassword());
 
-
-
-
-
-
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-};
-
-function generatePassword() {
-    let passwordLength = prompt("How many characters would you liek in your password? Must be between 8 and 128 characters.");
-    if (passwordLength >= 8 || passwordLength <= 128) {
-      let wantsSpecial = confirm("If you'd like special characters, click okay");
-      let wantsLower = confirm("If you'd like lowercase characters click okay.");
-      let wantsUpper = confirm("If you'd like uppercase characters click okay.");
-      let wantsNumber = confirm("If you'd like numbers click okay.");
-      if (wantsNumber || wantsLower || wantsSpecial || wantsUpper) {
-          let passwordText = "";
-          
-      }
-    } 
-}
-    generatePassword();
+generateBtn.addEventListener('click', writePassword);
